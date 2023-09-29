2a6934c19d06251d8d7bac0ef6726c3e08b8bf2e
Fern <81830808+deerfern9@users.noreply.github.com>
2023-09-29 15:02:12 +0300

    Update velodrome withdraw.py

--- velodrome withdraw.py
+++ velodrome withdraw.py
@@ -21,6 +21,7 @@ eth_web3 = Web3(Web3.HTTPProvider(ethereum_rpc))
 
 velodrome_contract = web3.eth.contract(address=web3.to_checksum_address(velodrome_address), abi=velodrome_abi)
 usdc_weth_lp_contract = web3.eth.contract(address=web3.to_checksum_address(usdc_weth_lp_address), abi=usdc_weth_lp_abi)
+dai_weth_lp_contract = web3.eth.contract(address=web3.to_checksum_address(dai_weth_lp_address), abi=dai_weth_lp_abi)
 usdc_dai_lp_contract = web3.eth.contract(address=web3.to_checksum_address(usdc_dai_lp_address), abi=usdc_dai_lp_abi)
 weth_contract = web3.eth.contract(address=web3.to_checksum_address(weth_address), abi=weth_abi)
 
@@ -131,6 +132,40 @@ def remove_liquidity_usdc_weth(private, address):
         return False
 
 
+def remove_liquidity_dai_weth(private, address):
+    try:
+        wrapped_eth_address = '0x4200000000000000000000000000000000000006'
+        dai_address = '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1'
+        stable = False
+        liquidity = dai_weth_lp_contract.functions.balanceOf(address).call()
+        amount_a_min = int(1 / get_eth_price() * 4.9)
+        amount_b_nin = int(4.9 * 1_000_000)
+        to = address
+        deadline = int(time.time() + 1800)
+        approve(private, dai_weth_lp_contract)
+        wait_normal_gwei()
+        tx = velodrome_contract.functions.removeLiquidity(
+            wrapped_eth_address, dai_address, stable, liquidity, amount_a_min, amount_b_nin, to, deadline
+        ).build_transaction({
+            'from': address,
+            'nonce': web3.eth.get_transaction_count(address),
+            'gasPrice': web3.eth.gas_price,
+            'chainId': web3.eth.chain_id,
+        })
+
+        tx_create = web3.eth.account.sign_transaction(tx, private)
+        tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+        new_print(address, f'Liquidity removed: {tx_hash.hex()}')
+        write_to_file('removed liquidity hashes .txt', f'{private};{address};{tx_hash.hex()}')
+        web3.eth.wait_for_transaction_receipt(tx_hash)
+        time.sleep(1)
+        return True
+    except Exception as err:
+        new_print(web3.eth.account.from_key(private).address, f'Error: {err}', is_error=True)
+        write_to_file('ERROR.txt', f'{private};{err}')
+        return False
+
+
 def remove_liquidity_usdc_dai(private, address):
     try:
         wrapped_eth_address = '0x7F5c764cBc14f9669B88837ca1490cCa17c31607'
@@ -169,6 +204,10 @@ def main():
         address = web3.eth.account.from_key(private).address
         if usdc_weth_lp_contract.functions.balanceOf(address).call() > web3.to_wei(0.00000001, 'ether'):
             if remove_liquidity_usdc_weth(private, address):
+                unwrap_eth(private, address, weth_contract)
+                time.sleep(random.randint(*delay))
+        elif dai_weth_lp_contract.functions.balanceOf(address).call() > web3.to_wei(0.00000001, 'ether'):
+            if remove_liquidity_dai_weth(private, address):
                 unwrap_eth(private, address, weth_contract)
                 time.sleep(random.randint(*delay))
         elif usdc_dai_lp_contract.functions.balanceOf(address).call() > web3.to_wei(0.00000005, 'ether'):
